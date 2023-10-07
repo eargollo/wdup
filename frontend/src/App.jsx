c4197caf3f3c4a6e23a14928183c65725c429867
Eduardo Argollo <eargollo@gmail.com>
2023-10-07 11:18:25 -0700

    Listing files in groupd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,22 +13,37 @@ function App() {
         Greet(name).then(updateResultText);
     }
 
+    function deleteFile(e) {
+        console.log(e)
+        e.preventDefault();
+    }
+
+    function textHighlight(e) {
+        console.log(e)
+        console.log(e.target.id)
+        e.target.classList.toggle("file-selected");
+        parent.document.getElementById(`filename_${e.target.id}`).classList.toggle("file-selected");
+    }
+
     return (
         <div id="App">
             <div id="input" className="input-box">
                 <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
                 <button className="btn" onClick={greet}>Greet</button>
             </div>
-            { resultText.map((item, index) => ( 
-                <form className={index%2?"odd":"even"}>
-                {item.map((file) => (
-                    <div className="file">
-                        <input type="checkbox" id={index} />
-                        <span>{file.path}</span>
+            <div id="result">
+                <button className='btn' onClick={deleteFile}>Delete selected</button>
+                { resultText.map((item, index) => ( 
+                    <div id={index} className={index%2?"odd":"even"}>
+                    {item.map((file, fid) => (
+                        <div className="file">
+                            <input type="checkbox" id={`${index}_${fid}`} key={file.name} onClick={textHighlight} />
+                            <span id={`filename_${index}_${fid}`}>{file.path}</span>
+                        </div>
+                    ))}
                     </div>
                 ))}
-                </form>
-            ))}
+            </div>
         </div>
     )
 }
